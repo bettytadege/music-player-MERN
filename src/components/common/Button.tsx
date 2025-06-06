81e91feb2cc1df00348ff61fe84f98e625468d42
bettytadege <bettytadege13@gmail.com>
2025-06-06 20:59:57 +0300

    feat:add a resuable button

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type ButtonProps={
+    children:React.ReactNode,
+    className:string,
+    onClick:()=>void
+}
+function Button({children,className,onClick}:ButtonProps) {
+  return (
+    <div className={className} onClick={onClick}>
+      {children}
+    </div>
+  )
+}
+
+export default Button
