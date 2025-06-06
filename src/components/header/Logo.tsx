088b08eb3f500f9c1c86e072f2322381670700db
bettytadege <bettytadege13@gmail.com>
2025-06-06 21:21:37 +0300

    feat:add logo to header

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <>
+      <div className="md:w-12 md:h-12 w-16 h-16 pt-1">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png"
+          alt=""
+        />
+      </div>
+    </>
+  );
+}
+
+export default Logo
