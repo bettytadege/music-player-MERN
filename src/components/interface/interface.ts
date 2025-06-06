9093be6a61666c5725d9fec2fd193be829a27d7c
bettytadege <bettytadege13@gmail.com>
2025-06-07 00:14:28 +0300

    feat:create interface

---
+++ src/components/interface/interface.ts
@@ -0,0 +1,36 @@
+export interface ClassName {
+  className: string;
+}
+export interface Fill {
+  fill: string;
+}
+export interface OnClick {
+  onClick: (event: React.MouseEvent<MouseEvent>) => void;
+}
+export interface IconsProps {
+  // fill: string;
+  className: string;
+  onClick: (event: React.MouseEvent<SVGElement, MouseEvent>) => void;
+}
+
+export type AlbumType = {
+  id: string;
+  albumtitle: string;
+  mix: string;
+  track: TrackType[];
+  poster_url: string;
+  format: string;
+  listened: number;
+  liked: boolean;
+  number_of_track: number;
+};
+
+export type TrackType = {
+  id: string;
+  trackNumber: number;
+  title: string;
+  duration: number;
+  track_url: string;
+};
+
+export type PlayListAlbumType =  {watched: number};
