d569f7f6c2b63b90c6ace24db1bb7b74aa18dad9
bettytadege <bettytadege13@gmail.com>
2025-06-06 20:53:37 +0300

    feat:add browser router and query client provider

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { PlayerContextProvider } from './context/PlayerContext.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter } from 'react-router-dom'
+const client= new QueryClient()
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+  <QueryClientProvider client={client}>
+ <PlayerContextProvider>
+  <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    </PlayerContextProvider>
+    </QueryClientProvider>
+)
