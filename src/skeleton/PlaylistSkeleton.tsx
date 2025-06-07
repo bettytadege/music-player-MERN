aead0affb164e856e1e7807a9939336ed17653fa
bettytadege <bettytadege13@gmail.com>
2025-06-07 13:07:28 +0300

    feat:add playlist skeleton

---
+++ src/skeleton/PlaylistSkeleton.tsx
@@ -0,0 +1,29 @@
+function PlaylistSkeleton() {
+  return (
+    <>
+        
+    <div className="relative flex justify-start  p-14">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5">
+      <div className="relative md:w-[260px] md:h-[240px] rounded-3xl  h-[200px] w-[220px] bg-gray-400 ">
+        <div className="object-cover object-left-top w-full h-full rounded-3xl" />
+      
+      </div>
+      <div className="relative md:w-[260px] md:h-[240px] rounded-3xl  h-[200px] w-[220px] bg-gray-400 ">
+        <div className="object-cover object-left-top w-full h-full rounded-3xl" />
+      
+      </div>
+      <div className="relative md:w-[260px] md:h-[240px] rounded-3xl  h-[200px] w-[220px] bg-gray-400 ">
+        <div className="object-cover object-left-top w-full h-full rounded-3xl" />
+      
+      </div>
+      <div className="relative md:w-[260px] md:h-[240px] rounded-3xl  h-[200px] w-[220px] bg-gray-400 ">
+        <div className="object-cover object-left-top w-full h-full rounded-3xl" />
+      </div>
+      </div>
+    </div>
+    </>
+    
+  );
+}
+
+export default PlaylistSkeleton;
